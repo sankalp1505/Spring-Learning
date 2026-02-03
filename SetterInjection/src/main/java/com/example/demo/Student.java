1265e14097b11ca7c91e33e137234a70a6159c5d
sankalp1505 <sr20229081@gmail.com>
2026-02-03 21:06:41 +0530

    Setter Injection

---
+++ SetterInjection/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	Laptop ref;
+	
+	void study() {
+		System.out.println("Student is Studying");
+	}
+	
+	void useLaptop() 
+	{
+		ref.work();
+	}
+
+	public Laptop getRef() {
+		return ref;
+	}
+	
+	@Autowired
+	public void setRef(Laptop ref) {
+		this.ref = ref;
+	}
+	
+	
+}
