9ec5f38d7236b795079b6e9a3611f2a34321c57d
sankalp1505 <sr20229081@gmail.com>
2026-02-13 00:38:29 +0530

    @pathVariable

--- Thymeleaf/src/main/java/com/example/demo/Student.java
+++ Thymeleaf/src/main/java/com/example/demo/Student.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Student {
 
 	
-	@GetMapping("/details")
-	String showDetail(Model m) {
-		m.addAttribute("message","Hello from Sankalp");
-		
+	@GetMapping("/details/{name}")
+	String showDetail(@PathVariable String name,Model m) {
+		m.addAttribute("message",name);
+	
 		return "details";
 	}
+	
+	
 }
