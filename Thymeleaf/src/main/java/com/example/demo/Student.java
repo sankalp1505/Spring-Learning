f86ed105154c138fb7e26ab4e75301691d1ca01d
sankalp1505 <sr20229081@gmail.com>
2026-02-13 12:44:27 +0530

    add student

--- Thymeleaf/src/main/java/com/example/demo/Student.java
+++ Thymeleaf/src/main/java/com/example/demo/Student.java
@@ -5,18 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/student")
 public class Student {
 
 	
-	@GetMapping("/details/{name}")
-	String showDetail(@PathVariable String name,Model m) {
-		m.addAttribute("message",name);
+	@GetMapping("/details")
+	String showDetail(@RequestParam String name,@RequestParam int age ,@RequestParam String gender) {
 	
-		return "details";
+		return name + " " + age + " "+ gender;
 	}
 	
 	
